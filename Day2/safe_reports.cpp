f602b8023dfaff426a3528b10f9645e56996d6c9
StavrosLzp <stavros.lazopoulos@gmail.com>
2024-12-03 18:46:19 +0200

    Day 2

--- Day2/safe_reports.cpp
+++ Day2/safe_reports.cpp
@@ -7,9 +7,34 @@
 
 using namespace std;
 
+
+int checkline(vector<int> Line){
+    int increasing = 0, decreasing = 0;
+
+    for (size_t i = 0 ; i < Line.size() - 1; i++){
+        if (Line[i] <= Line[i+1]) increasing = 1;
+        if (Line[i] >= Line[i+1]) decreasing = 1; 
+
+        if (abs(Line[i+1] - Line[i]) > 3){
+            increasing = 1;
+            decreasing = 1;
+        }
+    }
+
+    if ( (increasing == 1 and decreasing == 0) or (increasing == 0 and decreasing == 1) )
+        return 1;
+    else {
+        return 0;
+    }
+}
+
+
 int main() {
-    vector<int> L,R;
+    /* ------------ ------------ Part 1 ------------ ------------  */
     int result = 0;
+    string line;
+    int previous, current;
+    int increasing, decreasing = 0;
 
     // Read file into internal stream buffer to later itarate through each line
     ifstream file("input.txt");
@@ -19,9 +44,6 @@ int main() {
     }
 
     // Process each line
-    string line;
-    int previous, current;
-    int increasing, decreasing = 0;
     while (getline(file, line)) { 
         istringstream iss(line);
 
@@ -47,7 +69,44 @@ int main() {
 
     }
     
-    cout << result << endl;    
+    cout << "Part 1 : The Safety Score is " << result << endl; 
+
+
+    /* ------------ ------------ Part 2 ------------ ------------  */
+    result = 0;
+    int tempResult = 0;
+    vector<int> LineV, tempV;
+
+    ifstream file2("input.txt");
+    while (getline(file2, line)) { 
+        istringstream iss(line);
+
+        while (iss.eof() ==0){
+            iss >> current;
+            LineV.push_back(current);
+        }
+
+
+        if (checkline(LineV) == 1){
+            tempResult = 1;
+
+        }
+        else {
+            for (int i = 0 ; i < LineV.size() ; i++){
+                copy(LineV.begin(), LineV.end(), back_inserter(tempV));
+                tempV.erase(tempV.begin() + i);
+                // for (auto j : tempV) cout << j << " ";
+                // cout << endl;
+                if (checkline(tempV) == 1) tempResult = 1;
+                tempV.clear();
+            }
+        }
+        result += tempResult;
+        tempResult = 0;
+        LineV.clear();
+    }
+    
+    cout << "Part 2 : The Safety Score is " << result << endl; 
 
 
     
